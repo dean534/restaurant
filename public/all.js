7395f821b6303eb6c8f0a9e51425b7cfa285fce6
dean534 <d810218@gmail.com>
2019-02-21 02:02:31 +0800

    css remanage

--- public/all.js
+++ public/all.js
@@ -1,20 +1,20 @@
-const menuBtn = document.getElementById("header_btn");
-const headNav = document.getElementById("header_navbar");
-const productMenu = document.getElementsByClassName('product_menu_item')
-menuBtn.addEventListener("click", toggleMenu);
+// const menuBtn = document.getElementById("header_btn");
+// const headNav = document.getElementById("header_navbar");
+// const productMenu = document.getElementsByClassName('product_menu_item')
+// menuBtn.addEventListener("click", toggleMenu);
 
-for(let item of productMenu){
-    item.addEventListener("click",selectMenu)
-}
+// for(let item of productMenu){
+//     item.addEventListener("click",selectMenu)
+// }
 
-function toggleMenu () {
-headNav.classList.toggle("header_navbar--active")
-}
+// function toggleMenu () {
+// headNav.classList.toggle("header_navbar--active")
+// }
 
-function selectMenu (e) {
-    for(let item of productMenu){
-        item.classList.remove("product_menu_item--active")
-    }
-    e.target.classList.add('product_menu_item--active');
+// function selectMenu (e) {
+//     for(let item of productMenu){
+//         item.classList.remove("product_menu_item--active")
+//     }
+//     e.target.classList.add('product_menu_item--active');
 
-}
+// }
